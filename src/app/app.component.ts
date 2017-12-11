ea28685a2912600b745aac014a15bca8c271fee9
Marc Geurts <geurtsmarc@hotmail.com>
2017-12-11 10:06:21 +0100

    express on ssl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import {Helpers} from "./helpers";
     encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
-    title = 'app';
     globalBodyClass = 'm-page--loading-non-block m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-light m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default';
 
     constructor(private _router: Router) {
