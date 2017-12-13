4ad99c36035a7b518d1dc44ee058441f8cc0fd1e
Marc Geurts <geurtsmarc@hotmail.com>
2017-12-13 17:06:33 +0100

    add chat back

--- src/app/web/layouts/quick-sidebar/quick-sidebar.component.ts
+++ src/app/web/layouts/quick-sidebar/quick-sidebar.component.ts
@@ -1,61 +1,54 @@
-import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
-import { ChatService, UserService } from '../../../shared/services';
-import { ChatMessage, User } from '../../../shared/models';
+import {Component, OnInit, OnDestroy, ViewEncapsulation} from '@angular/core';
+import {ChatService, UserService} from '../../../shared/services';
+import {ChatMessage, User} from '../../../shared/models';
 
 @Component({
-	selector: "app-quick-sidebar",
-	templateUrl: "./quick-sidebar.component.html",
-	encapsulation: ViewEncapsulation.None,
-	providers: [ChatService]
+    selector: "app-quick-sidebar",
+    templateUrl: "./quick-sidebar.component.html",
+    encapsulation: ViewEncapsulation.None,
+    providers: [ChatService]
 })
 export class QuickSidebarComponent implements OnInit, OnDestroy {
 
-	messages: ChatMessage[] = [];
-	user: User;
-	message: string;
-	connection;
+    messages: ChatMessage[] = [];
+    user: User;
+    message: string;
+    connection;
 
-	constructor(private chatService: ChatService, private userService: UserService) {
-		// this.chatService.getUsers().subscribe(data => {
-		//     let user = data['user'];
-		//     if (data['event'] === 'left') {
-		//         console.log('User left: ' + user);
-		//     } else {
-		//         console.log('User joined: ' + user);
-		//     }
-		// });
-	}
+    constructor(private chatService: ChatService, private userService: UserService) {
 
-	sendMessage() {
-		// this.chatService.sendMessage({
-		//     author: this.user,
-		//     message: this.message,
-		//     datetime: '',
-		//     type: 'message'
-		// });
-		// this.message = '';
-	}
+    }
 
-	getUser() {
-		this.userService.getUser()
-			.subscribe(
-			data => {
-				this.user = data;
-			});
-	}
+    sendMessage() {
+        this.chatService.sendMessage({
+            author: this.user,
+            message: this.message,
+            datetime: '',
+            type: 'message'
+        });
+        this.message = '';
+    }
 
-	getInitialMessages() {
-		// this.connection = this.chatService.getMessages().subscribe(message => {
-		//     this.messages.push(message);
-		// });
-	}
+    getUser() {
+        this.userService.getUser()
+            .subscribe(
+                data => {
+                    this.user = data;
+                });
+    }
 
-	ngOnInit() {
-		this.getUser();
-		this.getInitialMessages();
-	}
+    getInitialMessages() {
+        this.connection = this.chatService.getMessages().subscribe(message => {
+            this.messages.push(message);
+        });
+    }
 
-	ngOnDestroy() {
-		// this.connection.unsubscribe();
-	}
+    ngOnInit() {
+        this.getUser();
+        this.getInitialMessages();
+    }
+
+    ngOnDestroy() {
+        this.connection.unsubscribe();
+    }
 }
