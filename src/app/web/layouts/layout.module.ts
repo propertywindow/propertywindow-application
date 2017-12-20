a72bfe14be06738ae659a324f8a0b3f4b78b49f9
Marc Geurts <geurtsmarc@hotmail.com>
2017-12-20 14:57:21 +0100

    update typescript

--- src/app/web/layouts/layout.module.ts
+++ src/app/web/layouts/layout.module.ts
@@ -1,47 +1,47 @@
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {LayoutComponent} from './layout/layout.component';
-import {HeaderNavComponent} from './header-nav/header-nav.component';
-import {PagesComponent} from '../pages/pages.component';
-import {AsideNavComponent} from './aside-nav/aside-nav.component';
-import {FooterComponent} from './footer/footer.component';
-import {QuickSidebarComponent} from './quick-sidebar/quick-sidebar.component';
-import {ScrollTopComponent} from './scroll-top/scroll-top.component';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {HrefPreventDefaultDirective} from '../../shared/directives/href-prevent-default.directive';
-import {UnwrapTagDirective} from '../../shared/directives/unwrap-tag.directive';
-import {MomentModule} from 'angular2-moment';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { PagesComponent } from '../pages/pages.component';
+import { AsideNavComponent } from './aside-nav/aside-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { QuickSidebarComponent } from './quick-sidebar/quick-sidebar.component';
+import { ScrollTopComponent } from './scroll-top/scroll-top.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HrefPreventDefaultDirective } from '../../shared/directives/href-prevent-default.directive';
+import { UnwrapTagDirective } from '../../shared/directives/unwrap-tag.directive';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
-    declarations: [
-        LayoutComponent,
-        HeaderNavComponent,
-        PagesComponent,
-        AsideNavComponent,
-        FooterComponent,
-        QuickSidebarComponent,
-        ScrollTopComponent,
-        HrefPreventDefaultDirective,
-        UnwrapTagDirective,
-    ],
-    exports: [
-        LayoutComponent,
-        HeaderNavComponent,
-        PagesComponent,
-        AsideNavComponent,
-        FooterComponent,
-        QuickSidebarComponent,
-        ScrollTopComponent,
-        HrefPreventDefaultDirective,
-        MomentModule,
-    ],
-    imports: [
-        FormsModule,
-        CommonModule,
-        RouterModule,
-        MomentModule,
-    ]
+	declarations: [
+		LayoutComponent,
+		HeaderNavComponent,
+		PagesComponent,
+		AsideNavComponent,
+		FooterComponent,
+		QuickSidebarComponent,
+		ScrollTopComponent,
+		HrefPreventDefaultDirective,
+		UnwrapTagDirective,
+	],
+	exports: [
+		LayoutComponent,
+		HeaderNavComponent,
+		PagesComponent,
+		AsideNavComponent,
+		FooterComponent,
+		QuickSidebarComponent,
+		ScrollTopComponent,
+		HrefPreventDefaultDirective,
+		MomentModule,
+	],
+	imports: [
+		FormsModule,
+		CommonModule,
+		RouterModule,
+		MomentModule,
+	]
 })
 export class LayoutModule {
 }
--- src/app/web/layouts/quick-sidebar/quick-sidebar.component.ts
+++ src/app/web/layouts/quick-sidebar/quick-sidebar.component.ts
@@ -1,70 +1,70 @@
-import {Component, OnInit, OnDestroy, ViewEncapsulation} from '@angular/core';
-import {ConversationService, UserService} from '../../../shared/services';
-import {Conversation, User} from '../../../shared/models';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { ConversationService, UserService } from '../../../shared/services';
+import { Conversation, User } from '../../../shared/models';
 
 @Component({
-    selector: "app-quick-sidebar",
-    templateUrl: "./quick-sidebar.component.html",
-    encapsulation: ViewEncapsulation.None,
-    providers: [ConversationService]
+	selector: "app-quick-sidebar",
+	templateUrl: "./quick-sidebar.component.html",
+	encapsulation: ViewEncapsulation.None,
+	providers: [ConversationService]
 })
 export class QuickSidebarComponent implements OnInit, OnDestroy {
 
-    conversation: Conversation[] = [];
-    user: User;
-    users: User[] = [];
-    message: string;
-    connection;
+	conversation: Conversation[] = [];
+	user: User;
+	users: User[] = [];
+	message: string;
+	connection;
 
-    constructor(private _conversationService: ConversationService, private _userService: UserService) {
+	constructor(private _conversationService: ConversationService, private _userService: UserService) {
 
-    }
+	}
 
-    // todo: check if logged in
-    // todo: add smilies, chatnoices
+	// todo: check if logged in
+	// todo: add smilies, chatnoices
 
-    sendMessage() {
-        this._conversationService.sendMessage({
-            author: this.user,
-            message: this.message,
-            datetime: '',
-            type: 'message'
-        });
-        this.message = '';
-    }
+	sendMessage() {
+		this._conversationService.sendMessage({
+			author: this.user,
+			message: this.message,
+			datetime: '',
+			type: 'message'
+		});
+		this.message = '';
+	}
 
-    getUser() {
-        this._userService.getUser()
-            .subscribe(
-                data => {
-                    this.user = data;
-                });
-    }
+	getUser() {
+		this._userService.getUser()
+			.subscribe(
+			data => {
+				this.user = data;
+			});
+	}
 
-    getColleagues() {
-        this._userService.getColleagues()
-            .subscribe(
-                data => {
-                    this.users = data;
-                });
-    }
+	getColleagues() {
+		this._userService.getColleagues()
+			.subscribe(
+			data => {
+				this.users = data;
+			});
+	}
 
 
-    getInitialMessages() {
-        this.connection = this._conversationService.getConversation().subscribe(message => {
-            this.conversation.push(message);
-        });
-    }
+	getInitialMessages() {
+		this.connection = this._conversationService.getConversation().subscribe(message => {
+			this.conversation.push(message);
+		});
+	}
 
-    ngOnInit() {
-        if (this._userService.verify()) {
-            this.getUser();
-            this.getColleagues();
-            this.getInitialMessages();
-        }
-    }
+	ngOnInit() {
+		if (this._userService.verify()) {
+			this.getUser();
+			this.getColleagues();
+			this.getInitialMessages();
+		}
+	}
 
-    ngOnDestroy() {
-        this.connection.unsubscribe();
-    }
+	ngOnDestroy() {
+		this.connection.unsubscribe();
+	}
 }
