211a90d5f3ed8f0b1e34c3f4f325d91a2a9b2a51
Marc Geurts <geurtsmarc@hotmail.com>
2018-01-05 16:55:54 +0100

    add counters

--- src/assets/app/js/dashboard.js
+++ src/assets/app/js/dashboard.js
@@ -59,6 +59,167 @@ let Dashboard = function() {
         cb(start, end, '');
     };
 
+
+    //== Daily Sales chart.
+    //** Based on Chartjs plugin - http://www.chartjs.org/
+    var dailySales = function() {
+        var chartData = {
+            labels: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5", "Label 6", "Label 7", "Label 8", "Label 9", "Label 10", "Label 11", "Label 12", "Label 13", "Label 14", "Label 15", "Label 16"],
+            datasets: [{
+                //label: 'Dataset 1',
+                backgroundColor: mUtil.getColor('success'),
+                data: [
+                    15, 20, 25, 30, 25, 20, 15, 20, 25, 30, 25, 20, 15, 10, 15, 20
+                ]
+            }, {
+                //label: 'Dataset 2',
+                backgroundColor: '#f3f3fb',
+                data: [
+                    15, 20, 25, 30, 25, 20, 15, 20, 25, 30, 25, 20, 15, 10, 15, 20
+                ]
+            }]
+        };
+
+        var chartContainer = $('#m_chart_daily_sales');
+
+        if (chartContainer.length == 0) {
+            return;
+        }
+
+        var chart = new Chart(chartContainer, {
+            type: 'bar',
+            data: chartData,
+            options: {
+                title: {
+                    display: false,
+                },
+                tooltips: {
+                    intersect: false,
+                    mode: 'nearest',
+                    xPadding: 10,
+                    yPadding: 10,
+                    caretPadding: 10
+                },
+                legend: {
+                    display: false
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                barRadius: 4,
+                scales: {
+                    xAxes: [{
+                        display: false,
+                        gridLines: false,
+                        stacked: true
+                    }],
+                    yAxes: [{
+                        display: false,
+                        stacked: true,
+                        gridLines: false
+                    }]
+                },
+                layout: {
+                    padding: {
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0
+                    }
+                }
+            }
+        });
+    }
+
+    //== Profit Share Chart.
+    //** Based on Chartist plugin - https://gionkunz.github.io/chartist-js/index.html
+    var profitShare = function() {
+        if ($('#m_chart_profit_share').length == 0) {
+            return;
+        }
+
+        var chart = new Chartist.Pie('#m_chart_profit_share', {
+            series: [{
+                value: 32,
+                className: 'custom',
+                meta: {
+                    color: mUtil.getColor('brand')
+                }
+            },
+                {
+                    value: 32,
+                    className: 'custom',
+                    meta: {
+                        color: mUtil.getColor('accent')
+                    }
+                },
+                {
+                    value: 36,
+                    className: 'custom',
+                    meta: {
+                        color: mUtil.getColor('warning')
+                    }
+                }
+            ],
+            labels: [1, 2, 3]
+        }, {
+            donut: true,
+            donutWidth: 17,
+            showLabel: false
+        });
+
+        chart.on('draw', function(data) {
+            if (data.type === 'slice') {
+                // Get the total path length in order to use for dash array animation
+                var pathLength = data.element._node.getTotalLength();
+
+                // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+                data.element.attr({
+                    'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+                });
+
+                // Create animation definition while also assigning an ID to the animation for later sync usage
+                var animationDefinition = {
+                    'stroke-dashoffset': {
+                        id: 'anim' + data.index,
+                        dur: 1000,
+                        from: -pathLength + 'px',
+                        to: '0px',
+                        easing: Chartist.Svg.Easing.easeOutQuint,
+                        // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+                        fill: 'freeze',
+                        'stroke': data.meta.color
+                    }
+                };
+
+                // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+                if (data.index !== 0) {
+                    animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+                }
+
+                // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+
+                data.element.attr({
+                    'stroke-dashoffset': -pathLength + 'px',
+                    'stroke': data.meta.color
+                });
+
+                // We can't use guided mode as the animations need to rely on setting begin manually
+                // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+                data.element.animate(animationDefinition, false);
+            }
+        });
+
+        // For the sake of the example we update the chart every time it's created with a delay of 8 seconds
+        chart.on('created', function() {
+            if (window.__anim21278907124) {
+                clearTimeout(window.__anim21278907124);
+                window.__anim21278907124 = null;
+            }
+            window.__anim21278907124 = setTimeout(chart.update.bind(chart), 15000);
+        });
+    }
+
+
     const latestTrendsMap = function() {
         if ($('#m_chart_latest_trends_map').length === 0) {
             return;
@@ -79,6 +240,8 @@ let Dashboard = function() {
         init: function() {
             daterangepickerInit();
             latestTrendsMap();
+            dailySales();
+            profitShare();
             // initTimer();
         }
     };
